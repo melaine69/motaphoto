71b8ed5822a6c8079b11755e5fdcfcefafe2eb33
Mélaine <113093304+melaine69@users.noreply.github.com>
2026-02-13 11:58:01 +0100

    Responsive single page

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
     <div class="footer-container">
             <ul id="footer-menu" class="menu">
                 <li>
-                    <a rel="privacy-policy" href="<?php echo esc_url(home_url('mentions-legales/')); ?>">Mentions Légales</a>
+                    <a href="#">Mentions Légales</a>
                 </li>
                 <li>
-                    <a href="<?php echo esc_url(home_url('privacy-policy/')); ?>">Vie Privée</a>
+                    <a href="#">Vie Privée</a>
                 </li>
                 <li>
-                    <a>Tous droits réservés</a>
+                    <a href="#">Tous droits réservés</a>
                 </li>
             
             </ul>
